7d0ce5e231b34677738f9e4375a05f6e5d6738e1
rumpelsmokeskin <joshnwatkins@gmail.com>
2025-03-14 19:46:40 -0700

    week 16 upload with a form in a card

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,65 @@
-import { useState } from "react"
-import SaunaView from "./assets/SaunaView"
-import SideBar from "./assets/SideBar"
-import ToolBar from "./assets/ToolBar"
-import testSauna from "./assets/Saunas"
-import blanksauna from "./assets/blanksauna.png"
-
-
+import { useState } from "react";
+import SaunaView from "./assets/SaunaView";
+import SideBar from "./assets/SideBar";
+import ToolBar from "./assets/ToolBar";
+import testSauna from "./assets/Saunas";
+import blanksauna from "./assets/blanksauna.png";
 
 export default function App() {
   //  **raised** state for sauna :)
   //TEST_DATA put into state
-  const [saunaState, setSaunaState] = useState(testSauna)
-  const [selectedSaunaID, setSelectedSaunaID] = useState(0)
+  const [saunaState, setSaunaState] = useState(testSauna);
+  const [selectedSaunaID, setSelectedSaunaID] = useState(0);
 
   //function section
 
   //function to add a new sauna
   const addNewSauna = () => {
     const newSauna = {
-        id: saunaState.length ? saunaState[saunaState.length - 1].id +1 : 0,
-        order: saunaState.length + 1,
-        image: blanksauna,
-        name: "New Sauna",
-        text: "New Sauna text",
-        isSelected: false,
-    }
-    setSaunaState([...saunaState, newSauna])
-  }
+      id: saunaState.length ? saunaState[saunaState.length - 1].id + 1 : 0,
+      order: saunaState.length + 1,
+      image: blanksauna,
+      name: "New Sauna",
+      text: "New Sauna text",
+      isSelected: false,
+    };
+    setSaunaState([...saunaState, newSauna]);
+  };
 
   //function to delete a sauna
   const deleteSauna = (id: number) => {
-    setSaunaState(saunaState.filter(s => s.id !== id))
-  }
+    setSaunaState(saunaState.filter((s) => s.id !== id));
+  };
 
   //function to update text of a sauna
-  const updateSaunaText = (id: number, text: string) => {
-    setSaunaState(saunaState.map(s => s.id === id  ? ({...s, text}): s))
-  }
+  const updateSauna = (
+    id: number,
+    text: string,
+    image: string,
+    name: string
+  ) => {
+    setSaunaState(
+      saunaState.map((s) => (s.id === id ? { ...s, text, image, name } : s))
+    );
+  };
 
   //render section of App
   return (
     <div className="d-flex vh-100 flex-column">
-    <ToolBar addNewSauna={addNewSauna}/>
+      <ToolBar addNewSauna={addNewSauna} />
       <div className="d-flex flex-grow-1">
-      <SideBar sauna={saunaState} selectedSaunaID={selectedSaunaID} setSelectedSaunaID={setSelectedSaunaID} deleteSauna={deleteSauna} />
-      <SaunaView sauna={saunaState} selectedSaunaID={selectedSaunaID} updateSaunaText={updateSaunaText}/> 
-      
+        <SideBar
+          sauna={saunaState}
+          selectedSaunaID={selectedSaunaID}
+          setSelectedSaunaID={setSelectedSaunaID}
+          deleteSauna={deleteSauna}
+        />
+        <SaunaView
+          sauna={saunaState}
+          selectedSaunaID={selectedSaunaID}
+          updateSaunaText={updateSauna}
+        />
       </div>
-  </div>
-  )
+    </div>
+  );
 }
--- src/assets/SaunaView.tsx
+++ src/assets/SaunaView.tsx
@@ -1,42 +1,97 @@
 import { useState } from "react";
-import { Sauna } from "./Saunas"
-import ToolButton from "./ToolButton"
-
+import { Sauna } from "./Saunas";
+import ToolButton from "./ToolButton";
 
 const cardStyle = {
-    height: '35rem',
-    width: '25rem',
-    center: 'text-center'
-}
+ 
+  width: "25rem",
+  center: "text-center",
+};
 
 interface SaunaViewProps {
-    sauna: Sauna[];
-    selectedSaunaID: number;
-    updateSaunaText: (id: number, text: string) => void;
-  }
+  sauna: Sauna[];
+  selectedSaunaID: number;
+  updateSaunaText: (
+    id: number,
+    text: string,
+    image: string,
+    name: string
+  ) => void;
+}
 /* this is the card view of which sauna will be selected.  It will display a large image of the sauna, it's name and descriptive text.  The toolbutton will take info from input text and update the sauna info */
-export default function SaunaView({ sauna, selectedSaunaID, updateSaunaText }: SaunaViewProps ) {
+export default function SaunaView({
+  sauna,
+  selectedSaunaID,
+  updateSaunaText,
+}: SaunaViewProps) {
+  const [inputText, setInputText] = useState("");
+  const [inputImage, setInputImage] = useState("");
+  const [inputName, setInputName] = useState("");
 
-    const [inputText, setInputText] = useState("")
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
 
-    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputText(event.target.value)
-    }
-    // Toolbutton below contains the prop to update saunaview contenrt
-    return(
-        
-        <div className="container d-flex flex-grow-1 flex-column justify-content-center">
-        <div className="card justify-content-center shadow-sm " style={ cardStyle }>
-  <img src= { sauna[selectedSaunaID].image } className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{ sauna[selectedSaunaID].name }</h5>
-    <p className="card-text">{ sauna[selectedSaunaID].text }</p>
-    <input className="me-2" type="text" onChange={handleInput}></input>
-    
-    <ToolButton buttonColor= "purple" buttonText="Update" onClick={(() => updateSaunaText(selectedSaunaID, inputText))}/>
-  </div>
-  <br></br>
-</div>
-</div>
-    )
+  const handleImageInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputImage(event.target.value);
+  };
+
+  const handleInputName = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputName(event.target.value);
+  };
+  // Toolbutton below contains the prop to update saunaview content
+  return (
+    <div className="container d-flex flex-grow-1 flex-column justify-content-center">
+      <div className="card justify-content-center shadow-sm " style={cardStyle}>
+        <img
+          src={sauna[selectedSaunaID].image}
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">{sauna[selectedSaunaID].name}</h5>
+          <p className="card-text">{sauna[selectedSaunaID].text}</p>
+
+          <form
+            className="border rounded p-3"
+            onSubmit={(event) => event.preventDefault()}
+          >
+            <label>Sauna Image: </label>
+            <br></br>
+            <input
+              className="me-2"
+              type="text"
+              onChange={handleImageInput}
+            ></input>
+            <br></br>
+            <label>Sauna Name: </label>
+            <br></br>
+            <input
+              className="me-2"
+              type="text"
+              onChange={handleInputName}
+            ></input>
+            <br></br>
+            <label>Sauna Text: </label>
+            <br></br>
+            <input className="me-2" type="text" onChange={handleInput}></input>
+
+            <ToolButton
+              buttonText="Update Sauna"
+              buttonColor="purple"
+              onClick={() =>
+                updateSaunaText(
+                  selectedSaunaID,
+                  inputText,
+                  inputImage,
+                  inputName
+                )
+              }
+            />
+          </form>
+        </div>
+        <br></br>
+      </div>
+    </div>
+  );
 }
